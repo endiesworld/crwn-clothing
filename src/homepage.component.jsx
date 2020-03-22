94afb46c1db68c8dbf3d843a780fd6f9fd1a34c0
endiesworld <endiesworld@gmail.com>
2020-03-22 14:33:18 +0100

    created project with create react app and added homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './homepage.style.scss'
+
+
+export default function homePage() {
+    return (
+        <div className='homepage'>
+            <div className = 'directory-menu'>
+                <div className= 'menu-item'> 
+                    <div className= 'content'>
+                        <h1 className= 'title'>HATS</h1>
+                        <span className = 'subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className= 'menu-item'> 
+                    <div className= 'content'>
+                        <h1 className= 'title'>HATS</h1>
+                        <span className = 'subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className= 'menu-item'> 
+                    <div className= 'content'>
+                        <h1 className= 'title'>HATS</h1>
+                        <span className = 'subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
